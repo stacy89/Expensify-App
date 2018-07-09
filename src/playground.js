5c7783642b8d529799e12fe2a90f4aac79a0cb94
Stacy <stacyareas@gmail.com>
2018-07-09 15:41:41 -0700

    play around with dispatch and subscribe using redux

--- src/playground.js
+++ src/playground.js
@@ -3,13 +3,19 @@ import { createStore } from "redux";
 const store = createStore((state = { count:0 }, action) => {
     switch(action.type) {
         case "INCREMENT":
+            const incrementBy = typeof action.incrementBy === "number" ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case "DECREMENT":
+            const decrementBy = typeof action.decrementBy === "number" ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
+        case "SET":
+            return {
+                count: action.count    
+            };    
         case "RESET":
             return {
                 count: 0
@@ -19,10 +25,13 @@ const store = createStore((state = { count:0 }, action) => {
     }
 });    
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
 
 store.dispatch({
-    type: "INCREMENT"
+    type: "INCREMENT",
+    incrementBy: 5
 });
 
 store.dispatch({
@@ -34,7 +43,15 @@ store.dispatch({
 });
 
 store.dispatch({
+    type: "DECREMENT",
+    decrementBy: 10
+});
+
+store.dispatch({
     type: "DECREMENT"
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: "SET",
+    count: 101
+});
