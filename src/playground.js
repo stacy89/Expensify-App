f5f8d816f3da8aeb252856f77244de583da15f3e
Stacy <stacyareas@gmail.com>
2018-07-09 16:00:39 -0700

    oject destructing es6

--- src/playground.js
+++ src/playground.js
@@ -1,57 +1,82 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const store = createStore((state = { count:0 }, action) => {
-    switch(action.type) {
-        case "INCREMENT":
-            const incrementBy = typeof action.incrementBy === "number" ? action.incrementBy : 1;
-            return {
-                count: state.count + incrementBy
-            };
-        case "DECREMENT":
-            const decrementBy = typeof action.decrementBy === "number" ? action.decrementBy : 1;
-            return {
-                count: state.count - decrementBy
-            };
-        case "SET":
-            return {
-                count: action.count    
-            };    
-        case "RESET":
-            return {
-                count: 0
-            };
-        default:
-            return state;   
+// const store = createStore((state = { count:0 }, action) => {
+//     switch(action.type) {
+//         case "INCREMENT":
+//             const incrementBy = typeof action.incrementBy === "number" ? action.incrementBy : 1;
+//             return {
+//                 count: state.count + incrementBy
+//             };
+//         case "DECREMENT":
+//             const decrementBy = typeof action.decrementBy === "number" ? action.decrementBy : 1;
+//             return {
+//                 count: state.count - decrementBy
+//             };
+//         case "SET":
+//             return {
+//                 count: action.count    
+//             };    
+//         case "RESET":
+//             return {
+//                 count: 0
+//             };
+//         default:
+//             return state;   
+//     }
+// });    
+
+// store.subscribe(() => {
+//     console.log(store.getState());
+// });
+
+// store.dispatch({
+//     type: "INCREMENT",
+//     incrementBy: 5
+// });
+
+// store.dispatch({
+//     type: "INCREMENT"
+// });
+
+// store.dispatch({
+//     type: "RESET"
+// });
+
+// store.dispatch({
+//     type: "DECREMENT",
+//     decrementBy: 10
+// });
+
+// store.dispatch({
+//     type: "DECREMENT"
+// });
+
+// store.dispatch({
+//     type: "SET",
+//     count: 101
+// });
+
+// const person = {
+//     name: "Stacy",
+//     age: 29,
+//     location: {
+//         city: "Berkeley",
+//         temp: 70
+//     }
+// };
+
+// const {name, age} = person;
+
+// console.log(`${name} is ${age}.`)
+
+const book = {
+    title: "Ego is th enemy", 
+    author: "Ryan Holiday",
+    publisher: {
+        name: "Penguin"
     }
-});    
+};
 
-store.subscribe(() => {
-    console.log(store.getState());
-});
+const {name: publisherName = "Self-Published"} = book.publisher;
 
-store.dispatch({
-    type: "INCREMENT",
-    incrementBy: 5
-});
-
-store.dispatch({
-    type: "INCREMENT"
-});
-
-store.dispatch({
-    type: "RESET"
-});
-
-store.dispatch({
-    type: "DECREMENT",
-    decrementBy: 10
-});
-
-store.dispatch({
-    type: "DECREMENT"
-});
-
-store.dispatch({
-    type: "SET",
-    count: 101
-});
+console.log(publisherName);
