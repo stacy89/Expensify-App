4ddb94aabf3dda5dc526bd89532f3fad86a2af9c
Stacy <stacyareas@gmail.com>
2018-07-12 15:33:20 -0700

    add sort method to sort if sortBy set to date or amount and sort accordingly

--- src/playground.js
+++ src/playground.js
@@ -233,6 +233,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === "date") {
+            return a.createdAt > b.createdAt ? 1 : -1;
+        } else if (sortBy === "amount") {
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 };
 
@@ -249,21 +255,21 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: "Rent", amount: 100, createdAt: 1000}));
+const expenseOne = store.dispatch(addExpense({ description: "Rent", amount: 100, createdAt: -21000}));
 const expenseTwo = store.dispatch(addExpense({ description: "coffee", amount: 300, createdAt: -1000}));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter("ffe"));
+// store.dispatch(setTextFilter("ffe"));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate(0));
 // store.dispatch(setStartDate());
-store.dispatch(setEndDate(999));
+// store.dispatch(setEndDate(999));
 
 const demoState = {
     expenses: [{
