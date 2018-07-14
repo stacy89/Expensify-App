0e884d4fcf17b72a554f12b0789e8989a030a67d
Stacy <stacyareas@gmail.com>
2018-07-14 16:12:11 -0700

    split code up into individual files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisiableExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water Bill" }));
+store.dispatch(addExpense({ description: "Gas Bill" }));
+
+// store.dispatch(setTextFilter("bill"));
+store.dispatch(setTextFilter("water"));
+
+const state = store.getState();
+const visiableExpenses = getVisiableExpenses(state.expenses, state.filters);
+console.log(visiableExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
 
