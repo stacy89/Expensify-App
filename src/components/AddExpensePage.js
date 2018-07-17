80bbdb01a90bf0e9cb8fa74569318edca142358d
Stacy <stacyareas@gmail.com>
2018-07-17 15:00:22 -0700

    create form for AddExpenses

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,7 +1,11 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
 const AddExpensePage = () => (
-    <div>Add</div>
+    <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm />
+    </div>
 );
 
 export default AddExpensePage;
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default class ExpenseForm extends React.Component {
+    state = {
+        description: "",
+        note: ""
+    }
+
+    onDescriptionChnage = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    };
+
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }));
+    };
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChnage}
+                    /> 
+                    <input
+                        type="number"
+                        placeholder="Amount"
+                    />
+                    <textarea
+                        placeholder="Add a note for your expense (optional)"
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    >
+                    </textarea>
+                    <button>Add Expense</button>       
+                </form>
+            </div>
+        )
+    }
+}
