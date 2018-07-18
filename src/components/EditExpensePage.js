fd00f8a96d50ad564fdd781513df064f91d04046
Stacy <stacyareas@gmail.com>
2018-07-17 17:45:31 -0700

    add hoc for EditExpensePage

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const EditExpensePage = (props) => {
+    console.log(props);
     return (
-        <div>edit</div>
+        <div>
+            Editing the expense with id of {props.match.params.id}
+        </div>
     );
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+    };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
