8786c4ac5961ee0a29ec2dff1218825d99823bc2
Stacy <stacyareas@gmail.com>
2018-07-20 18:43:45 -0700

    snapshot testing with dynamic components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -6,9 +6,7 @@ import Header from "../../components/Header";
 test("should render Header correctly", () => {
     const wrapper = shallow(<Header />);
     expect(wrapper).toMatchSnapshot();
-    // const renderer = new ReactShallowRenderer();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
+
 
  
